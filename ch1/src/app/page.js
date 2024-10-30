19896c0952e82ec2f58edba7e613f09386ebc9e1
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-10-30 22:15:04 +0530

    Commit

---
+++ ch1/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>hello NextJS</h1>
+      </main>
+      
+  );
+}
