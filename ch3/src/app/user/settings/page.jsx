1a97ac9ad4a500777787979ab6a8d0987bbd4296
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-11-08 21:22:17 +0530

    .

--- ch3/src/app/user/settings/page.jsx
+++ ch3/src/app/user/settings/page.jsx
@@ -1,12 +1,11 @@
 const Products = () => {
   return (
     <>
-      <h1>Products Page User</h1>
+      <h1>Products Page</h1>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In nemo fugit
-        aspernatur cum pariatur doloribus itaque corrupti quo, ut ipsam
-        molestiae praesentium quidem libero repellendus nisi esse dolor quas
-        sed.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolor
+        cumque sapiente delectus saepe magni adipisci consequatur pariatur eius,
+        fugit quam nam quo quos distinctio voluptas illo animi odit eligendi.
       </p>
     </>
   );
